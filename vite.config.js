b29a9cef4ad2cb7028ce18db383b92ee6d0748ec
rkyxung <kimgy071920@gmail.com>
2025-09-15 14:09:47 +0900

    404 error fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/react-portfolio/", // 여기에 Repository 경로를 추가
+
 })
